d1de28f546d21fcbcf2b9864b3d889793ab62f1d
Matvey <sinema3d00@gmail.com>
2024-09-26 10:38:52 +0300

    add scene changer logic for buttons and apply it

---
+++ Assets/Scripts/LoadingLogic/SceneChanger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] private Button _changeSceneButton;
+    [SerializeField] private Loader.Scene _concreteScene;
+    private void LoadGame()
+    {
+        Loader.Load(_concreteScene);
+        Time.timeScale = 1.0f;
+    }
+
+    private void OnEnable()
+    {
+        if (_changeSceneButton != null)
+        {
+            _changeSceneButton.onClick.AddListener(LoadGame);
+        }
+        else
+        {
+            Debug.LogError("No scene is existing");
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_changeSceneButton != null)
+        {
+            _changeSceneButton.onClick.RemoveListener(LoadGame);
+        }
+    }
+}
