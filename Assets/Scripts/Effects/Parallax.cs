020af8a24eceb171fdc5d34a12d663fe315e589b
Matvey <sinema3d00@gmail.com>
2024-09-26 09:29:21 +0300

    add background and parallax effect on it

---
+++ Assets/Scripts/Effects/Parallax.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float _speed = 0.5f;  // Скорость движения параллакса
+
+    private Material _material;
+    private Vector2 _offset;
+
+    void Start()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+
+        _offset = new Vector2(_speed, 0);
+    }
+
+    void Update()
+    {
+        _material.mainTextureOffset += _offset * Time.deltaTime;
+    }
+}
