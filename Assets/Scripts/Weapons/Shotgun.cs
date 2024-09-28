826705a14cc6ea628ee11eba73f3d4b43c5300a0
Matvey <sinema3d00@gmail.com>
2024-09-28 15:46:48 +0300

    the realization of shooting from shotgun
    I use raycast to optimize that process

--- Assets/Scripts/Weapons/Shotgun.cs
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -1,20 +1,43 @@
+using System.Collections;
 using UnityEngine;
 
 public class Shotgun : Weapon
 {
+    [SerializeField] private float _maxBulletDistance = 10f;
+    [SerializeField] private int _numberOfBullets = 3;
+    [SerializeField] private float _angleVariation = 10f; 
+
     public override void Shoot()
     {
-        GameObject bullet = BulletPool.GetBullet();
-        bullet.transform.position = transform.position;
-        bullet.SetActive(true);
+        float bulletSpeed = 10f;
 
-        if (bullet.TryGetComponent<Bullet>(out var bulletComponent))
+        for (int i = 0; i < _numberOfBullets; i++)
         {
-            bulletComponent.SetBulletPool(BulletPool);
+
+            float randomAngle = Random.Range(-_angleVariation, _angleVariation);
+            Vector2 bulletDirection = Quaternion.Euler(0, 0, randomAngle) * Vector2.right;
+
+            GameObject bullet = Instantiate(BulletPrefab, transform.position, Quaternion.identity);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            bulletRb.velocity = bulletDirection.normalized * bulletSpeed;
+
+            Debug.Log("Raycast shot fired in direction: " + bulletDirection);
+
+            StartCoroutine(DestroyBulletAfterDistance(bullet, bulletDirection, _maxBulletDistance));
         }
-        else
+    }
+
+    private IEnumerator DestroyBulletAfterDistance(GameObject bullet, Vector2 direction, float maxDistance)
+    {
+        float traveledDistance = 0f;
+        Vector2 initialPosition = bullet.transform.position;
+
+        while (traveledDistance < maxDistance)
         {
-            Debug.LogError("Bullet prefab does not have a Bullet component attached.");
+            traveledDistance = Vector2.Distance(initialPosition, bullet.transform.position);
+            yield return null;
         }
+
+        Destroy(bullet);
     }
 }
