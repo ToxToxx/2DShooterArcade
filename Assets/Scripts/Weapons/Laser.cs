7212e496ab008ef5dac911ad0717196c09e1333a
Matvey <sinema3d00@gmail.com>
2024-10-11 14:04:22 +0300

    add laser class

---
+++ Assets/Scripts/Weapons/Laser.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : Weapon
+{
+    [SerializeField] private float _maxBulletDistance = 10f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        SetShootingStrategy(new RifleShootingStrategy(_maxBulletDistance));
+    }
+}
