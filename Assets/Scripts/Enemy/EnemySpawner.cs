3d18924227810fbe8368353d37e37587c0860732
Matvey <sinema3d00@gmail.com>
2024-10-11 16:00:49 +0300

    add enemy spawner

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private EnemyStatsSO _enemyStatsSO;
+
+    private void Start()
+    {
+        GameObject enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+
+        enemy.GetComponent<EnemyHealthController>().EnemyStats = _enemyStatsSO;
+        enemy.GetComponent<EnemyMovementController>().EnemyStats = _enemyStatsSO;
+        enemy.GetComponent<EnemyDamageController>().EnemyStats = _enemyStatsSO;
+    }
+}
