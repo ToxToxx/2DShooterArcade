b0442390d8e8ba9af0ff2508683c57c05ed14027
Matvey <sinema3d00@gmail.com>
2024-11-01 15:31:21 +0300

    add new breakpoints

--- Assets/Scripts/EnemyBreakpointManager.cs
+++ Assets/Scripts/EnemyBreakpointManager.cs
@@ -3,18 +3,51 @@ using UnityEngine;
 
 public class EnemyBreakpointManager : MonoBehaviour
 {
-    [SerializeField] private int[] _breakPoints;
+    [SerializeField] private int[] _enemyIntervalBreakPoints;
+    [SerializeField] private int[] _weaponBreakPoints;
+    [SerializeField] private int[] _enemySpawnerBreakPoints;
     [SerializeField] private ScoreManager _scoreManager;
 
-    private int i = 0;
-    public static event EventHandler OnBreakPointHappened;
+    private int _enemyIntervalCounter = 0;
+    private int _weaponCounter = 0;
+    private int _spawnerCounter = 0;
+
+    public static event EventHandler OnEnemyIntervalBreakPointHappened;
+    public static event EventHandler OnEnemySpawnerBreakPointHappened;
+    public static event EventHandler OnWeaponBreakPointHappened;
 
     private void Update()
     {
-        if (i < _breakPoints.Length && _scoreManager.GetScore() == _breakPoints[i])
+        CheckIfEnemyIntervalBreakpointHappened();
+        CheckIfEnemySpawnBreakpointHappened();
+        CheckIfWeaponBreakpointHappened();
+    }
+
+    private void CheckIfEnemyIntervalBreakpointHappened()
+    {
+        if (_enemyIntervalCounter < _enemyIntervalBreakPoints.Length && _scoreManager.GetScore() == _enemyIntervalBreakPoints[_enemyIntervalCounter])
         {
-            OnBreakPointHappened?.Invoke(this, EventArgs.Empty);
-            i++;
+            OnEnemyIntervalBreakPointHappened?.Invoke(this, EventArgs.Empty);
+            _enemyIntervalCounter++;
         }
     }
+
+    private void CheckIfEnemySpawnBreakpointHappened()
+    {
+        if (_spawnerCounter < _enemySpawnerBreakPoints.Length && _scoreManager.GetScore() == _enemySpawnerBreakPoints[_spawnerCounter])
+        {
+            OnEnemySpawnerBreakPointHappened?.Invoke(this, EventArgs.Empty);
+            _spawnerCounter++;
+        }
+    }
+
+    private void CheckIfWeaponBreakpointHappened()
+    {
+        if (_weaponCounter < _weaponBreakPoints.Length && _scoreManager.GetScore() == _weaponBreakPoints[_weaponCounter])
+        {
+            OnWeaponBreakPointHappened?.Invoke(this, EventArgs.Empty);
+            _weaponCounter++;
+        }
+    }
+
 }
