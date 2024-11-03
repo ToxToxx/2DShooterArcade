15473be827d95b6bc206443f9ebe78f2f1fa1821
Matvey <sinema3d00@gmail.com>
2024-11-03 17:24:19 +0300

    adding lose screen to active when player died

---
+++ Assets/Scripts/LoseManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseManager : MonoBehaviour
+{
+    [SerializeField] private PlayerHealthController _healthController;
+    [SerializeField] private GameObject _loseCanvas;
+
+    private void OnEnable()
+    {
+        _healthController.OnPlayerDeath += _healthController_OnPlayerDeath;
+    }
+
+    private void _healthController_OnPlayerDeath(object sender, System.EventArgs e)
+    {
+        _loseCanvas.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        _healthController.OnPlayerDeath -= _healthController_OnPlayerDeath;
+    }
+}
